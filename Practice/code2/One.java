b4e1220ec721630d5d8edf77cfc18c9084b1fea1
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2024-12-01 19:17:07 +0600

    Create One.java

---
+++ Practice/code2/One.java
@@ -0,0 +1,10 @@
+class LearnFinal{
+    final int XYZ=10; 
+}
+
+public class One{
+    public static void main (String [] args) {
+        LearnFinal x=new LearnFinal();
+        System.out.println(x.XYZ);
+    }
+}
