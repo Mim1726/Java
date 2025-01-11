b36fd31b64d2978e04bcfa3cee6a40541310b399
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-11 11:19:56 +0600

    Create Main.java

---
+++ Code/Final's(previous)/code1/Main.java
@@ -0,0 +1,7 @@
+/* Write a program in Java to perform the following:
+Consider the parent class named Account. Design its two child classes named CurrentAccount and SavingsAccount.
+Child classes should have at least one additional meaningful variable declared within them, one parameterized
+constructor, and a toString method. The CurrentAccount maintains daily transactions, and provides a chequebook
+facility, but does not provide any interest. The SavingsAccount does not provide any chequebook but provides 
+yearly interest.
+*/
