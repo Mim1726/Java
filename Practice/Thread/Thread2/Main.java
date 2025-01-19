6ebc33f81d9d62441dbf99ba4bc700b1bf5ea8bc
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-19 17:27:29 +0600

    Update Main.java

--- Practice/Thread/Thread2/Main.java
+++ Practice/Thread/Thread2/Main.java
@@ -1,13 +1,18 @@
-class MyRunnable implements Runnable {
-    public void run() {
-        System.out.println("Thread is running.");
+
+class MyRunnable implements Runnable{
+    public void run(){
+        System.out.println("Thread is running: "+Thread.currentThread().getName());
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        MyRunnable r = new MyRunnable(); // Create a runnable object
-        Thread t = new Thread(r); // Pass the runnable to the thread
-        t.start(); // Start the thread
+public class Main{
+    public static void main(String[] args){
+        MyRunnable myRunnable=new MyRunnable();
+        Thread t1=new Thread(myRunnable);
+        t1.start();
     }
 }
+/*
+output:
+"Thread is running: Thread-0"
+*/
