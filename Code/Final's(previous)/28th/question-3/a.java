13b7e30be71416667fa2d805320f8b8c833b3b2c
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-21 21:55:09 +0600

    Create a.java

---
+++ Code/Final's(previous)/28th/question-3/a.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+
+        //multiplication of matrices
+        Scanner sc=new Scanner(System.in);
+        //row
+        int m=sc.nextInt();
+        //col1==row2
+        int n=sc.nextInt();
+        //col2
+        int p=sc.nextInt();
+
+        int[][] A=new int[m][n];
+        int[][] B=new int[n][p];
+        int[][] C=new int[m][p];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                A[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                B[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<p;j++){
+                C[i][j]=0;
+                for(int k=0;k<n;k++){
+                    C[i][j]+=A[i][k]*B[k][j];
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<p;j++){
+                if(j!=0) System.out.print(" ");
+                System.out.print(C[i][j]);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
