7f1aafc90930056b06e2633de169cb8672964b35
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-02-19 10:23:48 +0600

    Update a.java

--- Code/Final's(previous)/28th/question-3/a.java
+++ Code/Final's(previous)/28th/question-3/a.java
@@ -21,8 +21,8 @@ public class Main{
             }
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<p;j++){
                 B[i][j]=sc.nextInt();
             }
         }
